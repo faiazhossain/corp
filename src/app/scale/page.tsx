cab70db884acc6037dfea45d6fab410aad1e9ffa
Faiaz Hossain <faiazhossain10@gmail.com>
2024-01-07 21:47:52 +0600

    feat: plausible added

---
+++ src/app/scale/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+import scaleImg from "../../../public/scale.jpg";
+export default function PerformancePage() {
+  return (
+    <Hero
+      imgData={scaleImg}
+      imgAlt="steel factory"
+      title="Scale your app to Infinity"
+    />
+  );
+}
